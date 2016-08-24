a4cd2ad46c44a94dcaabd25a4ab4cd6551054d23
Guillermo Vargas <gavargas22@gmail.com>
2016-08-23 23:00:04 -0600

    URi Generator

--- parser.py
+++ parser.py
@@ -9,29 +9,31 @@ from bs4 import BeautifulSoup
 # desired_state = raw_input('Enter a State name: ')
 state_to_query_state = 57
 
+# A function that generates a URL to parse data given some parameters as required by db.
+def generate_url(page_number, country, date_range, state_id, keyword, entries_per_page):
+    # The base URL
+    url_base = "http://www.legacy.com/ns/obitfinder/obituary-search.aspx?"
+    db_parameters = "Page=" + page_number + "&countryid=" + country + "&daterange=" + date_range + "&stateid=" + state_id +  "&keyword=" + keyword + "&entriesperpage=" + entries_per_page
+    # Combine the two pieces to query URL
+    url_with_parameters = url_base + db_parameters
+    # Return the string
+    return url_with_parameters
 
 
 # This is the URL that is needed to query obituary database
 # Page=2&countryid=1&daterange=Last3Days&stateid=57&keyword=&entriesperpage=50
 
+# Pass the URL and Soupify the URL
 
 try :
-    web_page = urllib2.urlopen("http://www.legacy.com/ns/obitfinder/obituary-search.aspx?" + "Page=" + 1 + "&countryid="1&daterange=Last3Days&stateid=57&keyword=&entriesperpage=50").read()
+    web_page = urllib2.urlopen(generate_url("5", "1", "Last3Days", "57", "", "50")).read()
     soup = BeautifulSoup(web_page, "html5lib")
     # specific_content_object = soup.find('status', {'class':'vi'}).contents
-    specific_content_object = soup.find('status').contents
+    specific_content_object = soup.findAll("div", { "class" : "entryContainer" })
     print(specific_content_object)
 except urllib2.HTTPError :
     print("HTTPERROR!")
 except urllib2.URLError :
     print("URLERROR!")
 
-# A function that generates a URL to parse data given some parameters as required by db.
-def generate_url(page_number, country, date_range, state_id, keyword, entries_per_page):
-    # The base URL
-    url_base = "http://www.legacy.com/ns/obitfinder/obituary-search.aspx?"
-    db_parameters = "?Page=" + page_number + "&countryid=" + country + "&daterange=" + daterange + "&stateid=" + state_id +  "&keyword=" + keyword + "&entriesperpage=" + entries_per_page
-    # Combine the two pieces to query URL
-    url_with_parameters = url_base + db_parameters
-    # Return the string
-    return url_with_parameters
+print(soup)
