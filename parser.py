3693bb5703f029bf565c08a8b704eda5c443dad6
Guillermo Vargas <gavargas22@gmail.com>
2016-08-25 00:06:45 -0600

    Check if we only have an obituary

--- parser.py
+++ parser.py
@@ -2,8 +2,15 @@
 #!/usr/bin/env python
 
 import urllib2
-from bs4 import BeautifulSoup
+import re
+import pdb
+import unicodedata
+import csv
 
+
+from bs4 import BeautifulSoup
+# Regular expression to get the name only from the text
+name_extractor = re.compile('\b(?!Hello\b)\w+')
 # Get input from the user
 # The state is chosen by default
 # desired_state = raw_input('Enter a State name: ')
@@ -23,17 +30,44 @@ def generate_url(page_number, country, date_range, state_id, keyword, entries_pe
 # This is the URL that is needed to query obituary database
 # Page=2&countryid=1&daterange=Last3Days&stateid=57&keyword=&entriesperpage=50
 
-# Pass the URL and Soupify the URL
+# A function to request entries from the site.
+def request_records(page_number, country, date_range, state_id, keyword, entries_per_page):
+    try :
+        # Create an aray to store the names of the queried people
+        parsed_names = []
+        # Open the needed URL
+        web_page = urllib2.urlopen(generate_url(page_number, country, date_range, state_id, keyword, entries_per_page)).read()
+        # This is an example: web_page = urllib2.urlopen(generate_url("1", "1", "Last3Days", "57", "", "50")).read()
+        # Soupify the text
+        soup = BeautifulSoup(web_page, "html5lib")
+        # Extract the name container of the person
+        name_container = soup.findAll("div", { "class" : "obitName" })
 
-try :
-    web_page = urllib2.urlopen(generate_url("5", "1", "Last3Days", "57", "", "50")).read()
-    soup = BeautifulSoup(web_page, "html5lib")
-    # specific_content_object = soup.find('status', {'class':'vi'}).contents
-    data_containers = soup.findAll("div", { "class" : "entry" })
-    # Extract the name container of the person
-    name_container = soup.findAll("div", { "class" : "obitName" })
-    print(name_container[0])
-except urllib2.HTTPError :
-    print("HTTPERROR!")
-except urllib2.URLError :
-    print("URLERROR!")
+        # Loop through the items
+        for name in name_container:
+            # Convert unicode into string to process and then downcase
+            name_string = unicodedata.normalize('NFKD', name.a['title']).encode('ascii','ignore').lower()
+            # Recover only the name of the person and only obituaries.
+            # Check if the string contains an obituary
+            if 'obituary' in name_string:
+                name_string = name_string.strip('read obituary for ')
+                # Print the name just for debuggin purposes
+                print(name_string)
+                # Append string to array of names
+                parsed_names.append(name_string)
+        # Return a list
+        return parsed_names
+
+    except urllib2.HTTPError :
+        print("HTTPERROR!")
+    except urllib2.URLError :
+        print("URLERROR!")
+
+
+pdb.set_trace()
+parsed_names = request_records("1", "1", "Last3Days", "57", "", "50")
+pdb.set_trace()
+with open('extracted_data.csv', 'w+') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames = ["Name"], dialect='excel')
+    writer.writeheader()
+    writer.writerows({'Name' : parsed_names[row]} for row in range(0, len(parsed_names)))
